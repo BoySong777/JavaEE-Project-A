5cd983601b23cac2de5018a5ced78713909bd005
thu <mingfei.net@gmail.com>
2018-10-10 09:34:05 +0800

    Update

--- src/main/java/project/demo/action/UserAction.java
+++ src/main/java/project/demo/action/UserAction.java
@@ -1,19 +1,26 @@
 package project.demo.action;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.jasypt.util.password.StrongPasswordEncryptor;
 import project.demo.model.User;
 import project.demo.util.DB;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet(urlPatterns = "/user")
 public class UserAction extends HttpServlet {
@@ -97,19 +104,54 @@ public class UserAction extends HttpServlet {
     }
 
     private void signUp(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String email = req.getParameter("email").trim();
-
-        if (queryUserByEmail(email) != null) {
-            req.setAttribute("message", "Email is existed.");
-            req.getRequestDispatcher("sign-up.jsp").forward(req, resp);
-            return;
-        }
-
-        String username = req.getParameter("username").trim();
-        String password = req.getParameter("password");
-        StrongPasswordEncryptor strongPasswordEncryptor = new StrongPasswordEncryptor();
-        password = strongPasswordEncryptor.encryptPassword(password);
+        String email = null;
+        String username = null;
+        String password = null;
         String avatar = "default.png";
+
+        DiskFileItemFactory diskFileItemFactory = new DiskFileItemFactory();
+        ServletContext servletContext = req.getServletContext();
+        String attribute = "javax.servlet.context.tempdir";
+        File repository = (File) servletContext.getAttribute(attribute);
+        diskFileItemFactory.setRepository(repository);
+        ServletFileUpload servletFileUpload = new ServletFileUpload(diskFileItemFactory);
+
+        try {
+            List<FileItem> fileItems = servletFileUpload.parseRequest(req);
+            for (FileItem fileItem : fileItems) {
+                if (fileItem.isFormField()) {
+                    // fileItem 是普通的属性
+                    switch (fileItem.getFieldName()) {
+                        case "email":
+                            email = fileItem.getString();
+                            if (queryUserByEmail(email) != null) {
+                                req.setAttribute("message", "Email is existed.");
+                                req.getRequestDispatcher("sign-up.jsp").forward(req, resp);
+                                return;
+                            }
+                            break;
+                        case "username":
+                            username = fileItem.getString();
+                            break;
+                        case "password":
+                            password = fileItem.getString();
+                            StrongPasswordEncryptor strongPasswordEncryptor = new StrongPasswordEncryptor();
+                            password = strongPasswordEncryptor.encryptPassword(password);
+                            break;
+                        default:
+                            break;
+                    }
+                } else {
+                    // fileItem 是上传的文件
+                    avatar = fileItem.getName();
+                    // 保存到服务器
+                    File file = new File("D:/" + fileItem.getName());
+                    fileItem.write(file);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         Connection connection = DB.getConnection();
         PreparedStatement preparedStatement = null;
